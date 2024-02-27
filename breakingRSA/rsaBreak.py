e7704ba15985c1eb841d2992b594f34e6d217fd7
amnigam <king.amit95@gmail.com>
2024-02-27 11:54:55 +0530

    Adhering to DRY principles

--- breakingRSA/rsaBreak.py
+++ breakingRSA/rsaBreak.py
@@ -12,6 +12,26 @@ parser.add_argument('-p', '--port', type=int, default=443, help='Port number')
 parser.add_argument('-o', '--output', type=str, help='Output File Name')
 args = parser.parse_args() 
 
+def cracked(n,e,p,q):
+    if args.output: 
+        outPath = args.output       # Extract the name of the output file. 
+    else:
+        outPath = "priv_key.pem"
+
+    # First display Messages
+    print('[+] Looks like we were able to CRACK it! .... ')
+    print(f'[+] First prime number p = {p}')
+    print(f'[+] Second prime number q = {q}')
+    print(f'[+] Difference between primes p & q is: ', p-q) 
+    phi = (p-1) * (q-1) 
+    d = modinv.modinv(e,phi)        # Use Modulo Inverse module to derive value of d. required for private key generation. 
+    print('[+] Value of d is: ',d) 
+    priv_key = priv.genkey(n,e,d,p,q) 
+
+    with open(outPath, "w") as f: 
+        f. write(priv_key)
+    print('[+] Private Key is => \n',priv_key)
+
 if args.domain:
     domain = args.domain    # Extract the domain
     port = args.port        # Extract the port
@@ -21,14 +41,14 @@ if args.domain:
         port = int(port) 
     # print(port) 
 
-    n, e = cer.extract(domain)      # Retrieve n, e values for the public key in the certificate. 
+    n, e = cer.extract(domain, port)      # Retrieve n, e values for the public key in the certificate. 
     domCrack = input('Do you want to crack a domain? (yes/no) ')    # Establish, whether user wants to apply Fermat Factorization on Public Key. 
 
     if n != 0 and domCrack.upper()=='YES':
         try:
-            p,q = fer.fermat(n) 
-            print(p) 
-            print(q) 
+            p,q = fer.fermat(n)   
+            # If p, q value has been obtained it means the Public Key from Certificate was CRACKED
+            cracked(n,e,p,q)  
         except KeyboardInterrupt as e:          # Exit gracefully if user doesn't want to sit for centuries while cracking RSA :)
             print('User Interrupt received. Exiting...')    
 
@@ -40,7 +60,6 @@ if args.key:
     e = 0 
     d=0
     keyPath = args.key          # Extract the filename containing Key
-    outPath = args.output       # Extract the name of the output file. 
 
     with open(keyPath, "r") as f:
         key = f.read() 
@@ -52,16 +71,6 @@ if args.key:
 
     try:
         p, q = fer.fermat(n) 
-        print('[+] The p value is: ',p) 
-        print('[+] The q value is: ', q) 
-        print('[+] The difference between the keys is: ', p-q)
+        cracked(n,e,p,q) 
     except Exception as e:
         print('Error while working with the key! {e}') 
-
-    phi = (p-1)*(q-1) 
-    d = modinv.modinv(e,phi)        # Use Modulo Inverse module to derive value of d. required for private key generation. 
-    print('[+] Value of d is: ',d) 
-    priv_key = priv.genkey(n,e,d,p,q) 
-    with open(outPath, "w") as f: 
-        f. write(priv_key)
-    print('[+] Private Key is => \n',priv_key)
