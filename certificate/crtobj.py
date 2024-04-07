fbac3da567d0058e233dcf55e0d503106b893c20
amnigam <king.amit95@gmail.com>
2024-04-07 20:11:54 +0530

    Identity Search Added

--- certificate/crtobj.py
+++ certificate/crtobj.py
@@ -1,25 +1,40 @@
 import json 
+import os 
 
 class Cert:
     # Pass the response to CRT.SH as a string
-    def __init__(self, crtdata):
+    def __init__(self, crtdata, outfile):
         self.data = crtdata 
         self.domains = []
         self.id = []
+        self.outfile = outfile
     
     def extractDomains(self):
         # The below is a tricky line of code. It is a LIST COMPREHENSION. 
         # It appends to domains list a domain that is not already present. Note, we use JSON.LOADS to convert string into python structure. 
         [self.domains.append(x['name_value']) for x in json.loads(self.data) if x['name_value'] not in self.domains]
-        self.writeToFile("domain-list.txt", self.domains) 
+        self.writeToFile(self.outfile, self.domains) 
         
         [self.id.append(x['id']) for x in json.loads(self.data) if x['id'] not in self.id]
-        self.writeToFile('crt-id.txt', self.id) 
+        self.writeToFile('crt-id.txt', self.id)     # Hard coded value for Cert ID File name. 
 
         return self.domains 
     
     def writeToFile(self,fname,listObj): 
-        with open(fname,"w") as f:
+        basedir = os.path.abspath(os.path.dirname(__file__)) 
+        newFolder = 'CRT-Output'
+        path = os.path.join(basedir,newFolder) 
+        
+        isExists = os.path.exists(path) 
+        if not isExists:
+            try:
+                os.mkdir(path,0o700)   
+            except Exception as e:
+                print(f'Error creating the CRT output folder. {e}') 
+
+        path = os.path.join(path,fname)     # Get final path to output file. 
+
+        with open(path,"w") as f:
             for item in listObj:
                 if type(item) != 'str':
                     f.write(str(item))
--- certificate/crtsh.py
+++ certificate/crtsh.py
@@ -2,14 +2,41 @@ import requests
 from pprint import pprint as pp 
 import crtobj
 import argparse 
+import os
 
 url = 'https://crt.sh'
 
+def makeRequest(query):
+    r = requests.get(url,params=query) 
+    data = r.text
+    c = crtobj.Cert(data, args.output) 
+    d = c.extractDomains()
+    pp(d)
+    return c 
+
+def downloadCertificate(certObj):
+    cont = input('Do you wish to continue to extract certificates? (y/Y)')
+    if cont.upper() == 'Y':
+        crtid = certObj.getcrtid()
+        for cid in crtid:
+            q = {
+                "d":cid,        # To download certificate you need to send query parameter d with CRTID. 
+            }
+            basedir = os.path.abspath(os.path.dirname(__file__)) 
+            outFolder = 'CRT-Output'
+            path = os.path.join(basedir,outFolder,f'{cid}-cert.crt')    # Write inside CRT-Output folder in its own cert file. 
+
+            r = requests.get(url, params=q, stream=True)    # Use STREAM to download. 
+            if r.status_code == 200:
+                with open(path, 'wb') as b:    # Write it into its own certificate file. 
+                    b.write(r.raw.read())
+
 # Setting up the script's options. 
 parser = argparse.ArgumentParser(description='Script that crawls CRT.SH to obtain subdomains, certificates and other data.') 
-parser.add_argument('-cn', '--commonName', type=str, help='Common Name to be looked up')
+parser.add_argument('-cn', '--commonName', type=str, help='Search on the basis of Common Name (CN)')
+parser.add_argument('-i', '--identity', type=str, help='Search on the basis of Identity')
 parser.add_argument('-e', '--expired', action='store_false', default=True, help='Exclude Expired certificates. By default, it is True. Setting this flag makes it False.') 
-parser.add_argument('-o', '--output', type=str, help='Output File Name')
+parser.add_argument('-o', '--output', type=str, default='domain-list.txt', help='Output File Name to capture domain list.')
 args = parser.parse_args()
 
 if args.commonName:
@@ -25,23 +52,21 @@ if args.commonName:
             "CN": args.commonName,
             "output": "json"
         }
+    crt = makeRequest(q) 
+    downloadCertificate(crt)
 
-    r = requests.get(url,params=q) 
-    data = r.text
-    c = crtobj.Cert(data) 
-    d = c.extractDomains()
-    pp(d)
-
-    cont = input('Do you wish to continue to extract certificates? (y/Y)')
-
-    if cont.upper() == 'Y':
-        crtid = c.getcrtid()
-        for cid in crtid:
-            q = {
-                "d":cid,        # To download certificate you need to send query parameter d with CRTID. 
-            }
-
-            r = requests.get(url, params=q, stream=True)    # Use STREAM to download. 
-            if r.status_code == 200:
-                with open(f'{cid}-cert.crt', 'wb') as b:    # Write it into its own certificate file. 
-                    b.write(r.raw.read())
+if args.identity:
+    # The query structure is for a very basic query in JSON format for non-expired certificates for a supplied Identity Value. 
+    if (args.expired):
+        q = {
+            "Identity": args.identity,
+            "output": "json",
+            "exclude": "expired"
+        }
+    else:
+        q = {
+            "Identity": args.identity,
+            "output": "json"
+        }
+    crt = makeRequest(q) 
+    downloadCertificate(crt)    
